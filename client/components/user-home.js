2f0bace2a3011591f4c5c73afb3ca3b9c87c3234
Eric Guo <ericxguo@gmail.com>
2018-06-21 15:08:05 -0500

    changed where navbar was being rendered

--- client/components/user-home.js
+++ client/components/user-home.js
@@ -1,32 +1,26 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import {connect} from 'react-redux'
+import NavBar from './navbar'
 
 /**
  * COMPONENT
  */
 export const UserHome = props => {
-<<<<<<< HEAD
-  const { twitchLogin } = props.user
-  console.log(props.user)
-=======
-  const {email, twitchLogin} = props
->>>>>>> 6ae56fc5980d20474317645ec70ed68e3fa7c041
+  const {twitchLogin} = props
 
   return (
     <div>
+      <NavBar />
       <h3>Welcome, {twitchLogin}</h3>
-<<<<<<< HEAD
       {
-        props.isLoggedIn
+        props.twitchLogin
           ? <div>
               <h1>Connect your spotify account</h1>
               <a href="/auth/spotify">Connect</a>
             </div>
           : <div />
       }
-=======
->>>>>>> 6ae56fc5980d20474317645ec70ed68e3fa7c041
     </div>
   )
 }
@@ -37,13 +31,8 @@ export const UserHome = props => {
 const mapState = state => {
   console.log('state', state)
   return {
-<<<<<<< HEAD
-    user: state.user,
-    isLoggedIn: !!state.user.id
-=======
     email: state.user.email,
     twitchLogin: state.user.twitchLogin
->>>>>>> 6ae56fc5980d20474317645ec70ed68e3fa7c041
   }
 }
 
