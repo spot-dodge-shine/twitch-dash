2d5a926b04d0737b2c8edce83f491953a5b10bc9
Jake Myers <jakemyers2016@u.northwestern.edu>
2018-06-23 11:22:28 -0500

    IT FUCKING WORKS BITCHES

--- client/components/playlist-dropdown.js
+++ client/components/playlist-dropdown.js
@@ -27,12 +27,12 @@ export class PlaylistDropdown extends Component {
     super(props)
     if (this.props.user.spotifyAccessToken) {
       this.props.getPlaylists()
-        .then(() => {
-          return this.props.selectPlaylist(Object.keys(this.props.playlists)[0])
-        })
-        .then(() => {
-          return this.props.getTracks(this.props.selectedPlaylist())
-        })
+        // .then(() => {
+        //   return this.props.selectPlaylist(Object.keys(this.props.playlists)[0])
+        // })
+        // .then(() => {
+        //   return this.props.getTracks(this.props.selectedPlaylist())
+        // })
         .then(() => {
           this.timer = setInterval(this.tick, 5000)
           this.counter = 0
@@ -66,7 +66,6 @@ export class PlaylistDropdown extends Component {
       await this.props.getVotes(this.props.votecycle)
       await this.props.getPlayerStatus()
       if (!this.props.playerStatus.isPlaying && !this.props.playerStatus.progress) {
-        await this.props.deactivateVotecycle(this.props.votecycle.id)
         let newTrack
         let maxVotes = 0
         this.props.votecycle.votechoices.forEach(votechoice => {
@@ -76,6 +75,7 @@ export class PlaylistDropdown extends Component {
           }
         })
         console.log(newTrack)
+        await this.props.deactivateVotecycle(this.props.votecycle.id)
         return this.props.playTrack(newTrack)
       }
     }
@@ -83,7 +83,20 @@ export class PlaylistDropdown extends Component {
 
   handleChange = async (evt, data) => {
     await this.props.selectPlaylist(data.value)
-    return this.props.getTracks(this.props.selectedPlaylist())
+    await this.props.getTracks(this.props.selectedPlaylist())
+    if (this.props.votecycle && this.props.votecycle.id) {
+      await this.props.deactivateVotecycle(this.props.votecycle.id)
+    }
+    await this.props.createActiveVotecycle(this.props.user.id)
+    let choiceArr = []
+    const tracks = Object.values(this.props.tracks)
+    let myTrack, trackInd
+    for (let i = 0; i < this.props.numChoices; i++) {
+      trackInd = Math.floor(Math.random() * tracks.length)
+      myTrack = tracks[trackInd]
+      choiceArr.push(this.props.createVotechoice(this.props.votecycle.id, i + 1, myTrack) )
+    }
+    return Promise.all(choiceArr)  
   }
 
   handlePlay = async () => {
