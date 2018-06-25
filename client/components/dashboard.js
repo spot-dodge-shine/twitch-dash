df34d030e988172a8a8ce2e9466937328a802c78
Homum Ahsan <hahsan95@gmail.com>
2018-06-25 15:37:53 -0500

    dashboard changes

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -1,2 +1,31 @@
+import React, {Component} from 'react'
 import { allModules } from '../allModules'
+import Navbar from './navbar'
+
+class Dashboard extends Component {
+  render () {
+    return (
+      <div>
+        <Navbar />
+        {/*THIS IS THE SIDEBAR*/}
+        <div>
+        {
+          Object.keys(allModules).map(name => {
+            return <SideBarModule key={name} icon={""} description={""} name={name} />
+           })
+        }
+        </div>
+        <div>
+        {
+          Object.keys(allModules).map(name => {
+            return <DashModule key={name} icon={""} description={""} name={name} modules={allModules[name].dashboardComponent} />
+           })
+        }
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Dashboard
 
