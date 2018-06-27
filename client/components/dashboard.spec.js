e4c1e25dc6efa8df7f2c807b04860b27bf28e51b
Homum Ahsan <hahsan95@gmail.com>
2018-06-27 16:48:24 -0500

    dashboard test

---
+++ client/components/dashboard.spec.js
@@ -0,0 +1,22 @@
+import {expect} from 'chai'
+import React from 'react'
+import enzyme, {shallow} from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import Dashboard from './dashboard'
+import store from '../store'
+
+const adapter = new Adapter()
+enzyme.configure({adapter})
+
+describe.only('Dashboard', () => {
+  let dashboard
+
+  beforeEach(() => {
+    dashboard = shallow(
+      <Dashboard store={store} />)
+  })
+
+  xit('renders a div with components in it', () => {
+    expect(dashboard.find('div').exists()).to.equal(true)
+  })
+})
