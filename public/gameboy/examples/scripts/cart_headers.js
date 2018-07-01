ecea59fd4a2581d5f4e0c6bcabfdcc54a590ff75
Eric Guo <ericxguo@gmail.com>
2018-07-01 13:20:58 -0500

    fixed bug that changed the dir of the bundle.js file

--- public/gameboy/examples/scripts/cart_headers.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-const fs = require('fs');
-const Cart = require('../../lib/cart/cart');
-
-
-const cart = new Cart(fs.readFileSync('./roms/pokemon.gb'));
-console.log(JSON.stringify(cart.toJSON()));
