e8d0d4261f24ffb9a03f8765379b09020db43ee3
Eric Guo <ericxguo@gmail.com>
2018-07-01 13:13:02 -0500

    broke overlays :(

--- client/components/overlay-module.js
+++ client/components/overlay-module.js
@@ -2,6 +2,7 @@ import React from 'react'
 import {allModules} from '../allModules'
 
 const OverlayModule = (props) => {
+  console.log(props)
   const Module = allModules[props.match.params.moduleId].overlayComponent
   return <Module userId={props.match.params.userId}/>
 }
