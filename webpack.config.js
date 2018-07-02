f9e784cf67fa93700cd65864dc2cc1db4c474557
Homum Ahsan <hahsan95@gmail.com>
2018-07-02 15:08:31 -0500

    testing without loader

--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,13 @@ module.exports = {
         test: /\.jsx?$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
-      },
-      {
-        test: /.css$/,
-        loaders: [
-          'style-loader', 'css-loader',
-        ]
       }
+      // {
+      //   test: /.css$/,
+      //   loaders: [
+      //     'style-loader', 'css-loader',
+      //   ]
+      // }
     ]
   }
 }
