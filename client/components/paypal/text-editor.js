4aa69857ff516e3afd4b53ddaca874a006c58211
Homum Ahsan <hahsan95@gmail.com>
2018-07-02 16:34:09 -0500

    changing styles

--- client/components/paypal/text-editor.js
+++ client/components/paypal/text-editor.js
@@ -1,9 +1,7 @@
 import React, {Component} from 'react'
-import {EditorState, RichUtils} from 'draft-js'
 import Editor, { createEditorStateWithText } from 'draft-js-plugins-editor'
 import createToolbarPlugin from 'draft-js-static-toolbar-plugin'
-import {Grid, Menu} from 'semantic-ui-react'
-// import editorStyles from './editorStyle.css'
+// import editorStyles from '../../../public/style.css'
 import 'draft-js-static-toolbar-plugin/lib/plugin.css'
 import styled from 'styled-components'
 
@@ -12,8 +10,10 @@ const { Toolbar } = staticToolbarPlugin
 const plugins = [staticToolbarPlugin]
 const text = 'Your Billboard Message'
 
-const ToolbarStyle = styled.div`
-  display: flex;
+
+const EditorStyle = styled.div`
+  width: 425px;
+  height: auto;
 `
 
 class TextEditor extends Component {
@@ -33,7 +33,7 @@ class TextEditor extends Component {
 
   render() {
     return (
-      <div style={{width: '425px'}}>
+      <EditorStyle>
         <div className='editor' onClick={this.focus}>
           <Editor
             editorState={this.state.editorState}
@@ -41,11 +41,9 @@ class TextEditor extends Component {
             plugins={plugins}
             ref={(element) => { this.editor = element; }}
           />
-          <ToolbarStyle>
-            <Toolbar />
-          </ToolbarStyle>
+          <Toolbar />
         </div>
-      </div>
+      </EditorStyle>
     );
   }
 }
