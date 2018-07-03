896b13a465a0da569fb923addd02a90816252f3a
Eric Guo <ericxguo@gmail.com>
2018-07-03 10:20:30 -0500

    refactored to be ready without changes to ui

--- client/components/dashboard.js
+++ client/components/dashboard.js
@@ -13,32 +13,12 @@ class Dashboard extends Component {
   }
 
   render () {
-    const { active } = this.props.modules
-
     return (
       <div>
-      <Grid columns={2}>
-        <Grid.Row>
-          <Grid.Column>
-            {
-              active.indexOf(1) > -1
-                ? <DashModule module={allModules[1]} />
-                : <Card style={{ width: '425px', visibility: 'hidden' }} />
-            }
-          </Grid.Column>
-          <Grid.Column>
-            {
-              active.indexOf(2) > -1
-                ? <DashModule module={allModules[2]} />
-                : <Card style={{ width: '425px', visibility: 'hidden' }} />
-            }
-          </Grid.Column>
-        </Grid.Row>
-      </Grid>
         {
-          active.indexOf(3) > -1
-            ? <DashModule module={allModules[3]} />
-            : null
+          this.props.modules.active.map(id => {
+            return <DashModule key={id} module={allModules[id]} />
+          })
         }
       </div>
     )
